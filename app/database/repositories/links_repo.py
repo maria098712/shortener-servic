edadc17b98d3dfa3436b853092450e1774f5105d
Климанский Матвей <ak1986tutu@gmail.com>
2026-03-21 00:49:12 +0800

    0.3 feat: add func to increase clicks, add func to get link stats(clicks)

--- app/database/repositories/links_repo.py
+++ app/database/repositories/links_repo.py
@@ -1,4 +1,4 @@
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.models import Link
 from app.config import lg
@@ -11,7 +11,7 @@ class LinksRepository:
         self.db = db
 
     # Add new link
-    async def add_link(self, original_link: str, short_link: str, clicks: int):
+    async def add_link(self, original_link: str, short_link: str, clicks: int) -> None:
 
         stmt = (
             insert(Link)
@@ -25,12 +25,50 @@ class LinksRepository:
             lg.error(f"Error while trying to insert link ->:{error}")
 
     # Get redirect link by short
-    async def get_redirect_link(self, short_link: str):
+    async def get_redirect_link(self, short_link: str) -> str:
 
         stmt = (
             select(Link.original_link)
             .where(Link.short_link == short_link)
         )
+        try:
+            result = await self.db.execute(stmt)
+            redirect_link = result.scalar_one()
 
-        result = await self.db.execute(stmt)
-        redirect_link = result.scalar_one()
+            return redirect_link
+
+        except Exception as error:
+
+            lg.error(f"Error while trying to get redirect link ->:{error}")
+            return ""
+
+    # Increases the number of clicks by 1
+    async def increase_click(self, short_link: str) -> None:
+
+        stmt = (
+            update(Link)
+            .where(Link.short_link == short_link)
+            .values(clicks=Link.clicks + 1)
+        )
+
+        try:
+            await self.db.execute(stmt)
+
+        except Exception as error:
+            lg.error(f"Error while trying to update clicks ->:{error}")
+
+    # Get clicks number
+    async def  get_link_stats(self, short_link: str) -> int:
+        stmt = (
+            select(Link.clicks)
+            .where(Link.short_link == short_link)
+        )
+
+        try:
+            result = await self.db.execute(stmt)
+            clicks = result.scalar_one()
+            return clicks
+
+        except Exception as error:
+            lg.error(f"Error while trying to get clicks ->:{error}")
+            return 0
