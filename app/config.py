25eafeb7b3ea656760596fbcaa6bfa82d3f32c9a
Климанский Матвей <ak1986tutu@gmail.com>
2026-03-23 13:56:32 +0800

    1.2 feat: add check mode logic

--- app/config.py
+++ app/config.py
@@ -23,6 +23,8 @@ class Settings(BaseSettings):
     #tests
     TEST_BASE_URL: str
 
+
+
     @property
     def DB_URL(self) -> str:
         return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
--- app/database/connection.py
+++ app/database/connection.py
@@ -1,10 +1,17 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine,async_sessionmaker
 from app.config import settings
-from sqlalchemy.pool import NullPool
+from sqlalchemy.pool import NullPool, QueuePool
 
 
 DB_URL = settings.DB_URL
-engine = create_async_engine(DB_URL, echo=True, poolclass=NullPool)
+
+if settings.MODE == "TEST":
+    pool_class = NullPool
+
+else:
+    pool_class = QueuePool
+
+engine = create_async_engine(DB_URL, echo=True, poolclass=pool_class)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
